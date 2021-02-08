250e3836001834721026c3be9ad1b8b20a6cf7a8
Damien Scarpellini <damienscarpellini@yahoo.co.uk>
2021-02-08 14:38:40 +0000

    added new page for edit. added html for the nav to all php pages

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
-<?php
+<?php ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+</head>
+<body>
+    <div class="container">
+        <div class="logo-container"></div>
+        <div class="header-container"></div>
+        <div class="nav-container">
+            <a class="home-link">HOME</a>
+            <a class="all-link">ALL</a>
+            <a class="edit-link">EDIT</a>
+        </div>
+        <div class="welcome-container"></div>
+    </div>
+</body>
+</html>
